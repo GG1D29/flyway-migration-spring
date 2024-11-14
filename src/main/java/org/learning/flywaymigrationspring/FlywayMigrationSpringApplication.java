172fcdcf84468d4a4d80f7b1e8ab6173688e299c
GG <GG1D29@users.noreply.github.com>
2024-11-14 11:13:21 +0700

    frst

---
+++ src/main/java/org/learning/flywaymigrationspring/FlywayMigrationSpringApplication.java
@@ -0,0 +1,13 @@
+package org.learning.flywaymigrationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayMigrationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayMigrationSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/learning/flywaymigrationspring/FlywayMigrationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.flywaymigrationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayMigrationSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
